2ea71d04272c9cc25f0a2c63f7879bb19213fe34
Backku <bachnguynxun929@gmail.com>
2024-11-27 22:12:04 +0700

    Update

--- SieuThiMini/SieuThiMini/FormHoaDon.Designer.cs
+++ SieuThiMini/SieuThiMini/FormHoaDon.Designer.cs
@@ -41,79 +41,80 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 53);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 65);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(51, 19);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 83);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 108);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(51, 19);
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(30, 124);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(40, 156);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(51, 19);
             this.label3.TabIndex = 2;
             this.label3.Text = "label3";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(30, 166);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 204);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(51, 19);
             this.label4.TabIndex = 3;
             this.label4.Text = "label4";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(428, 381);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(571, 469);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(51, 19);
             this.label5.TabIndex = 4;
             this.label5.Text = "label5";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(11, 195);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(15, 240);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(555, 158);
+            this.dataGridView1.Size = new System.Drawing.Size(740, 194);
             this.dataGridView1.TabIndex = 6;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(228, 9);
+            this.label6.Location = new System.Drawing.Point(304, 11);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(107, 24);
+            this.label6.Size = new System.Drawing.Size(142, 31);
             this.label6.TabIndex = 7;
             this.label6.Text = "HÓA ĐƠN";
             // 
             // FormHoaDon
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 434);
+            this.ClientSize = new System.Drawing.Size(769, 534);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label5);
@@ -121,7 +122,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormHoaDon";
             this.Text = "FormHoaDon";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
--- SieuThiMini/SieuThiMini/FormHoaDon.cs
+++ SieuThiMini/SieuThiMini/FormHoaDon.cs
@@ -17,21 +17,20 @@ namespace SieuThiMini
         {
             InitializeComponent();
             CultureInfo cultureInfo = new CultureInfo("vi-VN");
-            // Khai báo các cột cho DataGridView nếu chưa có
             if (!dataGridView1.Columns.Contains("MaSP")) dataGridView1.Columns.Add("MaSP", "Mã Sản Phẩm");
             if (!dataGridView1.Columns.Contains("TenSP")) dataGridView1.Columns.Add("TenSP", "Tên Sản Phẩm");
             if (!dataGridView1.Columns.Contains("SoLuong")) dataGridView1.Columns.Add("SoLuong", "Số Lượng");
             if (!dataGridView1.Columns.Contains("Gia")) dataGridView1.Columns.Add("Gia", "Giá Bán");
             if (!dataGridView1.Columns.Contains("ThanhTien")) dataGridView1.Columns.Add("ThanhTien", "Thành Tiền");
 
-            // Gán thông tin cho các Label
-            label1.Text = "Mã Đơn Hàng: " + maDH;  // Label hiển thị mã đơn hàng
-            label2.Text = "Tên Khách Hàng: " + tenKH;  // Label hiển thị tên khách hàng
-            label3.Text = "Địa Chỉ: " + diaChi;  // Label hiển thị địa chỉ
-            label4.Text = "Ngày Mua: " + ngayMua.ToString("dd/MM/yyyy");  // Label hiển thị ngày mua
-            label5.Text = "Tổng Bill: " + tongHD.ToString("C", cultureInfo);  // Hiển thị tổng hóa đơn định dạng tiền tệ
 
-            // Điền dữ liệu vào DataGridView
+            label1.Text = "Mã Đơn Hàng : " + maDH;  
+            label2.Text = "Tên Khách Hàng : " + tenKH; 
+            label3.Text = "Địa Chỉ : " + diaChi;  
+            label4.Text = "Ngày Mua : " + ngayMua.ToString("dd/MM/yyyy"); 
+            label5.Text = "Tổng Bill : " + tongHD.ToString("C", cultureInfo);  
+
+
             foreach (DataGridViewRow row in orderDetails)
             {
                 if (row.Cells["MaSP"].Value != null)
@@ -39,16 +38,16 @@ namespace SieuThiMini
                     int maSP = Convert.ToInt32(row.Cells["MaSP"].Value);
                     string tenSP = row.Cells["TenSP"].Value.ToString();
                     int soLuong = Convert.ToInt32(row.Cells["SoLuong"].Value);
-                    decimal giaBan = Convert.ToDecimal(row.Cells["Gia"].Value);  // Lấy giá bán
+                    decimal giaBan = Convert.ToDecimal(row.Cells["Gia"].Value);  
                     decimal thanhTien = Convert.ToDecimal(row.Cells["ThanhTien"].Value);
 
-                    // Thêm dòng vào DataGridView
+
                     dataGridView1.Rows.Add(
                         maSP,
                         tenSP,
                         soLuong,
-                        giaBan.ToString("C", cultureInfo), // Định dạng giá bán
-                        thanhTien.ToString("C", cultureInfo) // Định dạng thành tiền
+                        giaBan.ToString("C", cultureInfo), 
+                        thanhTien.ToString("C", cultureInfo) 
                     );
                 }
             }
